bd7e563a9efd45888a2df4755184be2da39a928d
Ajitem Sahasrabuddhe <ajitem.sahasrabuddhe@perennialsys.com>
2019-01-25 23:11:17 +0530

    revert back the example update to default

---
+++ example/function/src/hello.php
@@ -0,0 +1,11 @@
+<?php
+
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: MIT-0
+
+function hello($data)
+{
+    return "Hello, {$data['name']}!";
+}
+
+?>
