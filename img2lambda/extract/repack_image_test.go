9b9813e2b1e80a962e2b69c7b544dbf7cef8add3
Clare Liguori <liguori@amazon.com>
2020-01-06 14:52:48 -0800

    Upgrade containers/image module

--- img2lambda/extract/repack_image_test.go
+++ img2lambda/extract/repack_image_test.go
@@ -14,7 +14,7 @@ import (
 
 	"github.com/awslabs/aws-lambda-container-image-converter/img2lambda/internal/testing/mocks"
 	"github.com/awslabs/aws-lambda-container-image-converter/img2lambda/types"
-	imgtypes "github.com/containers/image/types"
+	imgtypes "github.com/containers/image/v5/types"
 	"github.com/golang/mock/gomock"
 	"github.com/mholt/archiver"
 	godigest "github.com/opencontainers/go-digest"
--- img2lambda/internal/testing/generate_mocks.go
+++ img2lambda/internal/testing/generate_mocks.go
@@ -3,4 +3,4 @@
 package testing
 
 //go:generate mockgen.sh github.com/aws/aws-sdk-go/service/lambda/lambdaiface LambdaAPI mocks/lambda_mocks.go
-//go:generate mockgen.sh github.com/containers/image/types ImageCloser,ImageSource mocks/image_mocks.go
+//go:generate mockgen.sh github.com/containers/image/v5/types ImageCloser,ImageSource mocks/image_mocks.go
